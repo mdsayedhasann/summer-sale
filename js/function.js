bbb0dc89088ab8435b210e3943ec09ba1baa7bf7
sayed_hossain <sayed.hasan0011@gmail.com>
2023-08-21 00:38:12 +0600

    Product Add to cart and Name display

--- js/function.js
+++ js/function.js
@@ -1,3 +1,18 @@
-function addToCart(){
-    console.log('clicked');
+function addToCart(target){
+    let total = parseFloat(0);
+
+    const productName = target.childNodes[3].childNodes[7].innerText;
+    const selectedProductList = document.getElementById('selected-product-list');
+
+    const count = selectedProductList.childElementCount;
+    const h3 = document.createElement('h3');
+    h3.classList.add('font-semibold', 'text-xl', 'py-2')
+    h3.innerHTML = `${count + 1}. ${productName}`
+
+    selectedProductList.appendChild(h3)
+
+
+
+    const productPrice = parseFloat(target.childNodes[3].childNodes[9].childNodes[0].innerText)
+    console.log(productPrice);
 }
