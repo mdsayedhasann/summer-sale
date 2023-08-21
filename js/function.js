186c7d06e651e4f9120e8274f05b3fa1d2d9186b
sayed_hossain <sayed.hasan0011@gmail.com>
2023-08-21 14:33:03 +0600

    Coupon Fixed, Make Payment Working

--- js/function.js
+++ js/function.js
@@ -1,3 +1,8 @@
+let total = parseFloat(0);
+let calculateDiscount = parseFloat(0);
+let finalTotal = 0;
+const getPreviousTotal = document.getElementById('previous-total');
+
 function addToCart(target) {
 
     const productName = target.childNodes[3].childNodes[7].innerText;
@@ -11,43 +16,29 @@ function addToCart(target) {
     selectedProductList.appendChild(h3)
 
 
-    let total = parseFloat(0);
-    const getPreviousTotal = document.getElementById('previous-total');
+
+    
     const previousTotalString = getPreviousTotal.innerText;
     const previousPtotal = parseFloat(previousTotalString);
 
 
     const productPrice = parseFloat(target.childNodes[3].childNodes[9].childNodes[0].innerText);
     total = previousPtotal + productPrice;
-    getPreviousTotal.innerText = total.toFixed(2);
+    
+
+    // const inputCoupon = document.getElementById('input-coupon');
+    const couponApply = document.getElementById('coupon-apply');
+
+    if (total > 200) {
+        couponApply.removeAttribute('disabled');
+        couponApply.style.backgroundColor = '#e527b2';
+        console.log(couponApply.classList);
+
+    }
 
 
 
-    // Coupon & Discount 
-
-    document.getElementById('coupon-apply').addEventListener('click',function(){
-        const inputCoupon = document.getElementById('input-coupon');
-        const inputCouponValue = inputCoupon.value;
-        // console.log();
-
-
-        if(total > 200){
-            inputCoupon.setAttribute('enabled');
-            inputCoupon.classList.add('bg-black')
-
-        }
-
-        // if (total > 200 && inputCouponValue === 'SELL20') {
-        //     // Discount Calculation
-            
-        //     const getDiscount = document.getElementById('discount');
-        //     const calculateDiscount = total * .2;
-        //     getDiscount.innerText = calculateDiscount.toFixed(2);
-        // }
-    })
-
-
-
+    printTotal()
 
 
 
@@ -61,3 +52,70 @@ function addToCart(target) {
     // const calculateFinalTotalAmount = parseFloat(calculateFinalTotal)
     // console.log(calculateFinalTotalAmount);
 }
+
+
+// Coupon & Discount 
+
+document.getElementById('coupon-apply').addEventListener('click', function () {
+    const inputCoupon = document.getElementById('input-coupon');
+    const inputCouponValue = inputCoupon.value;
+    // console.log();
+
+
+
+    if (inputCouponValue === 'SELL20') {
+        // Discount Calculation
+        const getDiscount = document.getElementById('discount');
+        calculateDiscount = total * .2;
+        getDiscount.innerText = calculateDiscount.toFixed(2);
+    }
+
+    printTotal()
+
+
+})
+
+
+function printTotal() {
+    getPreviousTotal.innerText = total.toFixed(2);
+    finalTotal = total - calculateDiscount;
+    console.log(finalTotal);
+
+    const final = document.getElementById('final-total');
+    // const finalString = final.innerText;
+    // const finalAmount = parseFloat(finalString)
+
+    final.innerText = finalTotal;
+
+    if (finalTotal > 0) {
+        const makePaymentButton = document.getElementById('make-payment')
+        makePaymentButton.removeAttribute('disabled');
+        makePaymentButton.style.backgroundColor = '#E527B2'
+    }
+
+}
+
+// const closeButton = document.getElementById('goHome');
+// closeButton.addEventListener('click', goHome
+
+// )
+
+// function goHome() {
+//     total = 0;
+//     calculateDiscount = 0;
+//     printTotal();
+// }
+
+const closeButton = document.getElementById('goHome');
+closeButton.addEventListener('click', function(){
+    total = 0;
+    calculateDiscount = 0;
+    finalTotal = 0
+    printTotal();
+})
+
+// function goHome() {
+//     total = 0;
+//     calculateDiscount = 0;
+//     printTotal();
+// }
